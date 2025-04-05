8156aa34956e0807bf5944b7ab7fd54c8a4e8268
Linas Mickevicius <linasmnew@gmail.com>
2025-04-05 23:45:34 +0100

    Define checklist entity

--- src/checklists/entities/checklist.entity.ts
+++ src/checklists/entities/checklist.entity.ts
@@ -1,1 +1,20 @@
-export class Checklist {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Status } from '../checklists.enums';
+
+@Entity()
+export class Checklist {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    building: string;
+
+    @Column()
+    date: Date;
+
+    @Column({
+        type: 'enum',
+        enum: Status,
+    })
+    status: Status;   
+}
